d1ffc4d343569ac91c3811147d6a49fb5de0718e
Thiago Nitschke <thiago.nitschke@gmail.com>
2020-04-22 21:21:49 -0300

    Initial Commit

---
+++ ProjectRosieJetson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectRosieJetson
+//
+//  Created by Thiago Nitschke Simões on 22/04/20.
+//  Copyright © 2020 thnitschke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
